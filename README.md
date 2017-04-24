9104c491629e147756a612cef3c62a97821d0bfc
Jake Lindsey <jlindsey_84@hotmail.com>
2017-04-24 12:32:31 -0400

    init
    Re-uploading files

---
+++ README.md
@@ -0,0 +1,4 @@
+# Kromite's Public Cydia Repo - Created and Shared Packages
+# Reddit: Kromeee
+
+![Center-aligned image](https://github.com/Kromite/kromite.github.io/blob/master/CydiaIcon.png)
---
+++ packages.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+# dpkg-deb -bZgzip projects/<project name> <output folder>
+# dpkg-deb -bZgzip projects/BetteriPod debs
+# dpkg-deb -bZgzip projects/BootGlitch debs
+# dpkg-deb -bZgzip projects/CapitalLogo debs
+# dpkg-deb -bZgzip projects/CapitalBoot
+dpkg-deb -bZgzip projects/GBA4iOS debs
+dpkg-deb -bZgzip projects/RetroArch debs
+dpkg-deb -bZgzip projects/RetroArch9 debs
+dpkg-deb -bZgzip projects/RetroArch10 debs
