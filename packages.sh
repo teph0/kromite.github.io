5d4402f052ce18c300e726424e02b3a427c1a70c
Kromite <jlindsey_84@hotmail.com>
2017-06-06 18:53:01 -0400

    Cydia Repo Updates

--- packages.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-# dpkg-deb -bZgzip projects/<project name> <output folder>
-# dpkg-deb -bZgzip projects/BetteriPod debs
-# dpkg-deb -bZgzip projects/CapitalLogo debs
-dpkg-deb -bZgzip projects/GBA4iOS debs
-# dpkg-deb -bZgzip projects/RetroArch debs
-dpkg-deb -bZgzip projects/RetroArch10 debs
