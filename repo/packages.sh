7143065d6124605b058da261e7df88ff10e0bf96
Kromite <jlindsey_84@hotmail.com>
2017-08-14 22:57:36 -0400

    Test

--- repo/packages.sh
+++ repo/packages.sh
@@ -1,5 +1,3 @@
 # !/bin/bash
 dpkg-deb -bZgzip projects/GBA4iOS debs
-# dpkg-deb -bZgzip projects/RetroArch debs
-dpkg-deb -bZgzip projects/RetroArch10 debs
-dpkg-deb -bZgzip projects/RetroArch11 debs
+dpkg-deb -bZgzip projects/RetroArch164 debs
