a526f17e8c26bb0e232af1065765615105802091
Kromite <jlindsey_84@hotmail.com>
2017-08-17 10:24:09 -0400

    New RetroArch Versions

--- repo/packages.sh
+++ repo/packages.sh
@@ -1,3 +1,5 @@
 # !/bin/bash
 dpkg-deb -bZgzip projects/GBA4iOS debs
+dpkg-deb -bZgzip projects/RetroArch102 debs
+dpkg-deb -bZgzip projects/RetroArch130 debs
 dpkg-deb -bZgzip projects/RetroArch164 debs
