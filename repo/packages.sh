2c56a5a71e0c66b5445a403a65400d542d8946a8
Kromite <Kromite@users.noreply.github.com>
2018-06-11 23:04:31 -0400

    Update Repo

--- repo/packages.sh
+++ repo/packages.sh
@@ -1,5 +1,3 @@
 # !/bin/bash
-dpkg-deb -bZgzip projects/GBA4iOS debs
-dpkg-deb -bZgzip projects/RetroArch102 debs
-dpkg-deb -bZgzip projects/RetroArch130 debs
 dpkg-deb -bZgzip projects/RetroArch164 debs
+dpkg-deb -bZgzip projects/GBA4iOS debs
