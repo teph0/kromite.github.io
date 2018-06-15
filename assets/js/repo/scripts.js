689f5f9f974b4291987b5dd1cc844099fb47c5bb
Kromite <Kromite@users.noreply.github.com>
2018-06-15 00:08:09 -0400

    Fix

--- assets/js/repo/scripts.js
+++ assets/js/repo/scripts.js
@@ -1,42 +1,10 @@
-$(function () {
-	$("#cpanel").dblclick(function() {
-		var pwd = prompt("Password: ");
-		if (pwd == null || pwd == "") {
-			alert("Error - Nothing was entered.");
-		} else if (pwd == "alpine") {
-			window.location.href=""; //Add control panel here
-		} else {
-			alert("Error - Wrong password.");
-		}
-	});
-});
-
-var currentInnerHtml;
-var element = new Image();
-var elementWithHiddenContent = document.querySelector("#hide");
-var innerHtml = elementWithHiddenContent.innerHTML;
-
-element.__defineGetter__("id", function() {
-	currentInnerHtml = "";
-});
-
-setInterval(function() {
-	currentInnerHtml = innerHtml;
-	console.log(element);
-	console.clear();
-	elementWithHiddenContent.innerHTML = currentInnerHtml;
-}, 1000);
 
 function save() {
 	var cpuKey = document.getElementById('cpu').value;
 	localStorage.setItem("text", cpuKey);
 }
 
-function retrieve(){
+function retrieve() {
 	var text = localStorage.getItem("text");
 	document.getElementById('textDiv').innerHTML = text;
 }
-
-var github = window.top.href="";
-var youtube = window.top.href="";
-var ttg = window.top.href="";
